a83a535e7ae0ab54f888776da6fae0a92d55e9dc
Jimmy <kongjimmy06@gmail.com>
2019-12-12 12:52:59 -0500

    test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const db = require('./db')
 const cors = require('cors')
+var port = process.env.PORT || 5000
 
 const origin = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : 'https://expenseupdated.herokuapp.com/'
 
@@ -11,7 +12,7 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({origin}));
-app.listen(process.env.portserver, () => {  console.log('We are live on ' + process.env.portserver);});
+app.listen(port, () => {  console.log('We are live on ' + port);});
 
 
 app.post('/createuser', (req,res) => {
